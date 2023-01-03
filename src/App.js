5ed351ad51b43ac42b2eeda65376d61821e8c325
Cezar Vasconcelos <cezarvasconcelos.dev@gmail.com>
2023-01-03 19:00:29 -0300

    feat: add Footer

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import GlobalStyle from "./globalStyles";
 import { Home } from "./pages";
 
 export default function App() {
   return (
-    <>
+    <Router>
       <GlobalStyle />
       <Navbar />
       <Routes>
         {/* <Route index element={<Home />} /> */}
         <Route path="*" element={<Home />} />
       </Routes>
-    </>
+      <Footer />
+    </Router>
   );
 }
 

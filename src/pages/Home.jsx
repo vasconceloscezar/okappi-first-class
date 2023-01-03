bfbdd57b42b6d0a49ffefec898eb52266dedc8b7
Cezar Vasconcelos <cezarvasconcelos.dev@gmail.com>
2023-01-03 19:07:39 -0300

    feat: add Content component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,16 @@
+import Content from "../components/Content";
 import Features from "../components/Features";
 import Hero from "../components/Hero";
+import { heroOne, heroTwo, heroThree } from "../data/HeroData";
 
 export function Home() {
   return (
     <>
       <Hero />
       <Features />
+      <Content {...heroOne} />
+      <Content {...heroTwo} />
+      <Content {...heroThree} />
     </>
   );
 }

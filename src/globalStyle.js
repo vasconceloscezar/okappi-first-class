77e7162325ed9c9464da2716f065ee17f5b006e9
Cezar Vasconcelos <cezarvasconcelos.dev@gmail.com>
2023-01-03 17:53:28 -0300

    feat: add Container style

--- src/globalStyle.js
+++ src/globalStyle.js
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
   *{
@@ -6,6 +6,19 @@ const GlobalStyle = createGlobalStyle`
   margin: 0;
   padding: 0;
   font-family: 'Montserrat', sans-serif;
+	background-color: slateblue;
+  }
+`;
+
+export const Container = styled.div`
+  width: 100%;
+  max-width: 1300px;
+  margin-right: auto;
+  margin-left: auto;
+  padding: 0 50px;
+
+  @media screen and (max-width: 960px) {
+    padding: 0 30px;
   }
 `;
 

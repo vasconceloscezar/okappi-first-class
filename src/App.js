547932f63ff0f51241ff8cf412526106fb36faa5
Cezar Vasconcelos <cezarvasconcelos.dev@gmail.com>
2023-01-03 17:31:48 -0300

    feat: add global styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Routes, Route } from "react-router-dom";
+import GlobalStyle from "./globalStyle";
 import { Home } from "./pages";
 
 export default function App() {
   return (
     <>
+      <GlobalStyle />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="*" element={<NoMatch />} />
